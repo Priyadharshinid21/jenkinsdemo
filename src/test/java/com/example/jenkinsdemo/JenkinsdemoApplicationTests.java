f3c10b415d430add640084a04c4c21eaa0b89b75
Priyadharshini <dharshinihelianthus21@gmail.com>
2024-01-11 19:25:30 -0800

    first test jenkin project committed

---
+++ src/test/java/com/example/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.jenkinsdemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdemoApplicationTests {
+	@Test
+	void contextLoads() {
+		assertEquals(true,true);
+	}
+
+}
